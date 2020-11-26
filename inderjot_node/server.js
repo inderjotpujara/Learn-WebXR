42b0bd19100daffabc4090a9502dd8feb10a3c84
unknown <inderjot.singh@trukker.com>
2020-11-26 17:51:15 +0530

    now serving files dynamically with nodeJs

--- inderjot_node/server.js
+++ inderjot_node/server.js
@@ -10,7 +10,7 @@ const options = {
   key: fs.readFileSync('key.pem'),
   cert: fs.readFileSync('cert.pem')
 };
-
+app.use('/static', express.static(__dirname + '/..'));
 app.use(express.static(__dirname + '/..'));
 
 
@@ -18,28 +18,22 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/:type/:lecture_number', (req, res) => {
+app.get('/lecture/:type/:lecture_number', (req, res) => {
+    console.log( path.join(__dirname + `/../${req.params.type}/lecture${req.params.lecture_number}/index.html`));
     fs.readFile(
         path.join(__dirname + `/../${req.params.type}/lecture${req.params.lecture_number}/index.html`),
             (error, pgResp) => {
                 if (error) {
-                    resp.writeHead(404);
-                    resp.write('Contents you are looking are Not Found');
+                    res.writeHead(404);
+                    res.write(`Lecture ${req.params.lecture_number} of type ${req.params.type} does not exists`);
                 } else {
-                    resp.writeHead(200, { 'Content-Type': 'text/html' });
-                    resp.write(pgResp);
+                    console.log(pgResp)
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.write(pgResp);
             }
-         
-                resp.end();
+                res.end();
             });
-
-    // console.log(path.join(__dirname + `/../${req.params.type}/lecture${req.params.lecture_number}/index.html`))
-    // res.sendFile(path.join(__dirname + `/../${req.params.type}/lecture${req.params.lecture_number}/index.html`))
 });
-
-// app.listen(port, () => {
-//     console.log(`Example app listening at http://localhost:${port}`)
-// })
 
 var httpsServer = https.createServer(options, app);
 
